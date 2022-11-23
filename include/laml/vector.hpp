c4b62b6cdb74ee1cdb726293c0a96fe36b653484
bitgraf <ablaskov27@gmail.com>
2022-11-23 16:43:33 -0600

    add vector functionality

--- include/laml/vector.hpp
+++ include/laml/vector.hpp
@@ -21,13 +21,21 @@ namespace rh::laml {
 		static_assert(size > 0, "Vector cannot be zero-size");
 
 		typedef T Type;
+		constexpr inline size_t num_elements() const { return size; }
 
 		// Default constructor
-		constexpr Vector() : data{0} {}
+		constexpr Vector() : _data{0} {}
+
+		// Construct with float array
+		Vector(const float* in_data) {
+			for (size_t n = 0; n < size; n++) {
+				_data[n] = static_cast<T>(in_data[n]);
+			}
+		}
 
 		// Initialize with list of components
 		template<class... U, class V = typename std::enable_if<sizeof...(U) + 1 == size, T>::type>
-		constexpr Vector(T first, U... next) noexcept : data{ first, next... } {}
+		constexpr Vector(T first, U... next) noexcept : _data{ first, next... } {}
 
 		// Initialize with single value for all components
 		//template<class U, class V = typename std::enable_if<std::is_same<T, U>::value && size != 1, T>::type>
@@ -35,16 +43,201 @@ namespace rh::laml {
 
 		// access like an array
 		T& operator[](size_t idx) {
-			return data[idx];
+			return _data[idx];
 		}
 		const T& operator[](size_t idx) const {
-			return data[idx];
+			return _data[idx];
 		}
 
-		T data[size];
+		/* Operators
+		* typename T needs to implement: +,-,*,/,==
+		* These are all component-wise operations
+		* */
+		Vector<T,size> operator+(const Vector<T,size>& other) const {
+			Vector<T,size> res;
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] + other[n];
+			}
+			return res;
+		}
+		Vector<T, size> operator-(const Vector<T, size>& other) const {
+			Vector<T, size> res;
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] - other[n];
+			}
+			return res;
+		}
+		Vector<T, size> operator*(const Vector<T, size>& other) const {
+			Vector<T, size> res;
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] * other[n];
+			}
+			return res;
+		}
+		Vector<T, size> operator/(const Vector<T, size>& other) const {
+			Vector<T, size> res;
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] / other[n];
+			}
+			return res;
+		}
+		bool operator==(const Vector<T, size>& other) const {
+			for (size_t n = 0; n < size; n++) {
+				if (_data[n] != other[n]) return false;
+			}
+			return true;
+		}
+
+		// scalar operators
+		Vector<T, size> operator*(const T& factor) const {
+			Vector<T, size> res;
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] * factor;
+			}
+			return res;
+		}
+		Vector<T, size> operator/(const T& factor) const {
+			Vector<T, size> res;
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] / factor;
+			}
+			return res;
+		}
+
+		// comparison operators
+		Vector<bool, size> operator>(const Vector<T,size>& other) const {
+			Vector<bool, size> res; // fill with falses?
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] > other[n];
+			}
+			return res;
+		}
+		Vector<bool, size> operator<(const Vector<T, size>& other) const {
+			Vector<bool, size> res; // fill with falses?
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] < other[n];
+			}
+			return res;
+		}
+		Vector<bool, size> operator>=(const Vector<T, size>& other) const {
+			Vector<bool, size> res; // fill with falses?
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] >= other[n];
+			}
+			return res;
+		}
+		Vector<bool, size> operator<=(const Vector<T, size>& other) const {
+			Vector<bool, size> res; // fill with falses?
+			for (size_t n = 0; n < size; n++) {
+				res[n] = _data[n] <= other[n];
+			}
+			return res;
+		}
+
+		const T* data() const { return _data; }
+		T* data() { return _data; }
+
+	private:
+		T _data[size];
 	};
 
+	// Free functions
+	template<typename T, size_t size>
+	T dot(const Vector<T, size>& v1, const Vector<T, size>& v2) {
+		T res = 0;
+		for (size_t n = 0; n < size; n++) {
+			res = res + (v1[n] * v2[n]);
+		}
+		return res;
+	}
+
+	template<typename T>
+	Vector<T,3> cross(const Vector<T, 3>& v1, const Vector<T, 3>& v2) {
+		Vector<T, 3> res;
+		res[0] = v1[1] * v2[2] - v1[2] * v2[1];
+		res[1] = v1[2] * v2[0] - v1[0] * v2[2];
+		res[2] = v1[0] * v2[1] - v1[1] * v2[0];
+		return res;
+	}
+
+	template<typename T, size_t size>
+	T length_sq(const Vector<T, size>& v) {
+		T res = 0;
+		for (size_t n = 0; n < size; n++) {
+			res = res + (v[n] * v[n]);
+		}
+		return res;
+	}
+
+	template<typename T, size_t size>
+	T length(const Vector<T, size>& v) {
+		return static_cast<T>(sqrt(length_sq(v)));
+	}
+
+	template<typename T, size_t size>
+	Vector<T,size> normalize(const Vector<T, size>& v) {
+		T mag = length(v);
+		return (v / mag);
+	}
+
+	template<typename T, size_t size>
+	T min(const Vector<T, size>& v) {
+		T min_val = v[0];
+		for (size_t n = 1; n < size; n++) {
+			if (v[n] < min_val) min_val = v[n];
+		}
+		return min_val;
+	}
+
+	template<typename T, size_t size>
+	T max(const Vector<T, size>& v) {
+		T max_val = v[0];
+		for (size_t n = 1; n < size; n++) {
+			if (v[n] > max_val) max_val = v[n];
+		}
+		return max_val;
+	}
+
+	template<typename T, size_t size>
+	Vector<T, size> abs(const Vector<T, size>& v) {
+		Vector<T, size> res;
+		for (size_t n = 0; n < size; n++) {
+			res[n] = v[n] > static_cast<T>(0.0) ? v[n] : -v[n];
+		}
+		return res;
+	}
+
+	template<typename T, size_t size>
+	Vector<T, size> clamp(const Vector<T, size>& v, T min_val, T max_val) {
+		Vector<T, size> res;
+		for (size_t n = 0; n < size; n++) {
+			res[n] = v[n] > max_val ? max_val : (v[n] < min_val ? min_val : v[n]);
+		}
+		return res;
+	}
+
+	template<typename T, size_t size>
+	Vector<T,size> lerp(const Vector<T, size>& v1, const Vector<T, size>& v2, T factor) {
+		return v2 * factor + v1 * (static_cast<T>(1.0) - factor);
+	}
+
+	template<size_t size>
+	bool any(const Vector<bool, size>& v) {
+		for (size_t n = 0; n < size; n++) {
+			if (v[n]) return true;
+		}
+		return false;
+	}
+	template<size_t size>
+	bool all(const Vector<bool, size>& v) {
+		for (size_t n = 0; n < size; n++) {
+			if (!v[n]) return false;
+		}
+		return true;
+	}
+
 	// Specializations
+#if 0
 #pragma warning(disable : 4201) // complains about unnammed union structs
 	template<typename T> struct Vector<T, 2> {
 		constexpr Vector() : data{0,0} {}
@@ -97,6 +290,7 @@ namespace rh::laml {
 		}
 	};
 #pragma warning(default : 4201)
+#endif
 
 
 	// Useful shorthands
--- test/main.cpp
+++ test/main.cpp
@@ -8,25 +8,90 @@
 
 using namespace rh;
 
+void print_vec(const laml::Vec2& vec) { printf("(%5.2f,%5.2f)", vec[0], vec[1]); }
+void print_vec(const laml::Vec3& vec) { printf("(%5.1f,%5.1f,%5.1f)", vec[0], vec[1], vec[2]); }
+void print_vec(const laml::Vec4& vec) { printf("(%5.1f,%5.1f,%5.1f,%5.1f)", vec[0], vec[1], vec[2], vec[3]); }
+void print(float f) { printf("(%5.2f)", f); }
+
+#define PLUS   printf(" + ");
+#define MINUS  printf(" - ");
+#define TIMES  printf(" x ");
+#define DIVIDE printf(" / ");
+#define EQUALS printf(" = ");
+#define DOT printf(" * ");
+#define CROSS printf(" X ");
+#define NL printf("\n");
+
 int main(int argc, char** argv) {
 	UNREFERENCED_PARAMETER(argc);
 	UNREFERENCED_PARAMETER(argv);
 
 	printf("laml version %s\n\n", laml::getVersionString());
 
-	laml::Vec2 v2(2.0f, 3.5f);
-	laml::Vec3 v3(1.0f, 2.0f, 3.0f);
-	laml::Vec4 v4(1.0f, 2.0f, 3.0f, 4.0f);
+	laml::Vec2 v1(0.0f, 3.5f);
+	laml::Vec2 v2(2.5f, -1.0f);
+	laml::Vec3 v3(1.0f, 2.0f, -2.5f);
+	laml::Vec3 v4(2.5f, -1.0f, 0.5f);
 
-	printf("(v2[0],v2[1]) = (%.2f %.2f)\n", v2[0], v2[1]);
-	printf("(v3[0],v3[1],v3[2]) = (%.2f %.2f,%.2f)\n", v3[0], v3[1], v3[2]);
-	printf("(v4[0],v4[1],v4[2],v4[3]) = (%.2f %.2f,%.2f,%.2f)\n", v3[0], v3[1], v3[2],v4[2]);
+	// Operators
+	print_vec(v1 + v2); EQUALS print_vec(v1); PLUS  print_vec(v2); NL;
+	print_vec(v1 - v2); EQUALS print_vec(v1); MINUS print_vec(v2); NL;
+	print_vec(v1 * v2); EQUALS print_vec(v1); TIMES  print_vec(v2); NL;
+	print_vec(v1 / v2); EQUALS print_vec(v1); DIVIDE print_vec(v2); NL;
+	NL;
 
-	printf("\n");
+	// Free functions
+	print(dot(v1, v2)); EQUALS print_vec(v1); DOT    print_vec(v2); NL;
+	print_vec(cross(v3, v4)); EQUALS print_vec(v3); CROSS print_vec(v4); NL;
+	print(length_sq(v3)); EQUALS printf("sum: "); print_vec(v3); DOT print_vec(v3); NL;
+	print(length(v3)); EQUALS printf("sqsum: "); print_vec(v3); DOT print_vec(v3); NL;
+	print_vec(normalize(v3)); EQUALS printf("|"); print_vec(v3); printf("|");
 
-	printf("(v2.x,v2.y) = (%.2f %.2f)\n", v2.x, v2.y);
-	printf("(v3.x,v3.y,v3.z) = (%.2f %.2f,%.2f)\n", v3.x, v3.y, v3.z);
-	printf("(v4.x,v4.y,v4.z,v4.w) = (%.2f %.2f,%.2f,%.2f)\n", v4.x, v4.y, v4.z, v4.w);
+	NL;
+	print_vec(v3); NL;
+	v3 = normalize(v3);
+	print_vec(v3); NL;
+	NL;
+
+	// min/max/clamp/lerp
+	laml::Vec4 v5(2.5f, -2.25f, 7.5f, 7.5001f);
+	print_vec(v5); NL;
+	printf("min(v5) = %f\n", min(v5));
+	printf("max(v5) = %f\n", max(v5));
+	printf("abs(v5) = "); print_vec(abs(v5)); NL;
+	NL;
+
+	printf("v1 = "); print_vec(v1); NL;
+	printf("v2 = "); print_vec(v2); NL;
+	for (float f = 0.0f; f < 1.05f; f += 0.1f) {
+		printf(" [%.2f] = ", f); print_vec(lerp(v1, v2, f)); NL;
+	}
+	printf("Clamped between 1.0 and 2.0\n");
+	for (float f = 0.0f; f < 1.05f; f += 0.1f) {
+		printf(" [%.2f] = ", f); print_vec(clamp(lerp(v1, v2, f), 1.0f, 2.0f)); NL;
+	}
+	NL;
+
+	// initialize with float array
+	float data[] = { 0.12f, 12.31f, 8.3f, 1.31f, 131.2f, 0.31f, 2.83f, 18.9f, 7.31f };
+	laml::Vector<float, 9> data_vec(data);
+	auto _data = data_vec.data();// get underlying data
+	for (size_t n = 0; n < data_vec.num_elements(); n++) {
+		printf("[%7.3f] -> [%7.3f]|[%7.3f]\n", data[n], (float)data_vec[n], (float)_data[n]);
+	}
+	NL;
+
+	auto res = v4 > v3;
+	printf("v3 = "); print_vec(v3); NL;
+	printf("v4 = "); print_vec(v4); NL;
+	printf("v4 > v3 = [");
+	for (size_t n = 0; n < res.num_elements(); n++) {
+		printf("%s ", res[n] ? "true" : "false");
+	}
+	printf("]\n");
+
+	printf("any(v4 > v3) = %s", any(res) ? "true" : "false"); NL;
+	printf("all(v4 > v3) = %s", all(res) ? "true" : "false"); NL;
 
 	//system("pause");
 
