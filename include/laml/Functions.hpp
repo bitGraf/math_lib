8155868111addfb962250670c7afbf95c3a6252b
bitgraf <ablaskov27@gmail.com>
2023-07-01 14:30:20 -0500

    safe versions of inverse trig functions

--- include/laml/Functions.hpp
+++ include/laml/Functions.hpp
@@ -21,6 +21,11 @@ namespace laml {
             return -value;
     }
 
+    template <typename T> 
+    int sign(T val) {
+        return (T(0) < val) - (val < T(0));
+    }
+
     template<typename T>
     T clamp(T& v, T min_val, T max_val) {
         return v > max_val ? max_val : (v < min_val ? min_val : v);
@@ -35,21 +40,106 @@ namespace laml {
     T sin(T x) {
         return  ::sin(x);
     }
-
     template<typename T>
     T sind(T x) {
         return  ::sin(x * laml::constants::deg2rad<T>);
     }
-
     template<typename T>
     T cos(T x) {
         return  ::cos(x);
     }
-
     template<typename T>
     T cosd(T x) {
         return  ::cos(x * laml::constants::deg2rad<T>);
     }
+    template<typename T>
+    T tan(T x) {
+        return  ::tan(x);
+    }
+    template<typename T>
+    T tand(T x) {
+        return  ::tan(x * laml::constants::deg2rad<T>);
+    }
+
+
+    template<typename T>
+    T asin(T x) {
+        return  ::asin(x);
+    }
+    template<typename T>
+    T asind(T x) {
+        return  ::asin(x) * laml::constants::rad2deg<T>;
+    }
+    template<typename T>
+    T acos(T x) {
+        return  ::acos(x);
+    }
+    template<typename T>
+    T acosd(T x) {
+        return  ::acos(x) * laml::constants::rad2deg<T>;
+    }
+    template<typename T>
+    T atan(T x) {
+        return  ::atan(x);
+    }
+    template<typename T>
+    T atand(T x) {
+        return  ::atan(x) * laml::constants::rad2deg<T>;
+    }
+    template<typename T>
+    T atan2(T x, T y) {
+        return  ::atan2(x, y);
+    }
+    template<typename T>
+    T atan2d(T x, T y) {
+        return  ::atan2(x, y) * laml::constants::rad2deg<T>;
+    }
+
+    // safe versions within a tolerance
+    template<typename T>
+    T asin_safe(T x, T tol) {
+        if (x > T( 1.0) && x < (T( 1.0) + tol)) x = T( 1.0);
+        if (x < T(-1.0) && x > (T(-1.0) - tol)) x = T(-1.0);
+        return  ::asin(x);
+    }
+    template<typename T>
+    T asind_safe(T x, T tol) {
+        if (x > T( 1.0) && x < (T( 1.0) + tol)) x = T( 1.0);
+        if (x < T(-1.0) && x > (T(-1.0) - tol)) x = T(-1.0);
+        return  ::asin(x) * laml::constants::rad2deg<T>;
+    }
+    template<typename T>
+    T acos_safe(T x, T tol) {
+        if (x > T( 1.0) && x < (T( 1.0) + tol)) x = T( 1.0);
+        if (x < T(-1.0) && x > (T(-1.0) - tol)) x = T(-1.0);
+        return  ::acos(x);
+    }
+    template<typename T>
+    T acosd_safe(T x, T tol) {
+        if (x > T( 1.0) && x < (T( 1.0) + tol)) x = T( 1.0);
+        if (x < T(-1.0) && x > (T(-1.0) - tol)) x = T(-1.0);
+        return  ::acos(x) * laml::constants::rad2deg<T>;
+    }
+    template<typename T>
+    T atan_safe(T x, T tol) {
+        if (x > T( 1.0) && x < (T( 1.0) + tol)) x = T( 1.0);
+        if (x < T(-1.0) && x > (T(-1.0) - tol)) x = T(-1.0);
+        return  ::atan(x);
+    }
+    template<typename T>
+    T atand_safe(T x, T tol) {
+        if (x > T( 1.0) && x < (T( 1.0) + tol)) x = T( 1.0);
+        if (x < T(-1.0) && x > (T(-1.0) - tol)) x = T(-1.0);
+        return  ::atan(x) * laml::constants::rad2deg<T>;
+    }
+    //template<typename T>
+    //T atan2_safe(T x, T y) {
+    //    return  ::atan2(x, y);
+    //}
+    //template<typename T>
+    //T atan2d_safe(T x, T y) {
+    //    return  ::atan2(x, y) * laml::constants::rad2deg<T>;
+    //}
 
 }
 
